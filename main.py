7c57edcaa8b08c6785c52da253d320bbc50b7188
Haresh Gautham S <haresh.gautham.s@ibm.com>
2024-04-15 01:16:48 +0530

    second commit

--- main.py
+++ main.py
@@ -44,9 +44,9 @@ def display_sentiment_summary(commentslst):
         else:
             neutral_count += 1
 
-    positive_percentage = (positive_count / num_comments) * 100
-    neutral_percentage = (neutral_count / num_comments) * 100
-    negative_percentage = (negative_count / num_comments) * 100
+    positive_percentage = (positive_count / num_comments+1) * 100
+    neutral_percentage = (neutral_count / num_comments+1) * 100
+    negative_percentage = (negative_count / num_comments+1) * 100
 
     st.title("Sentiment Summary")
     st.write(f"Total Comments: {num_comments}")
